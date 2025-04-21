cccf89f8789395525882ac9ad964495d6f81334f
alexeymartinez-encore <amartinez@encoreautomation.com>
2025-04-21 15:29:45 -0400

    Update discover.py

--- discover.py
+++ discover.py
@@ -4,3 +4,19 @@ with PLC() as comm:  # No IP address needed for broadcasting
     devices = comm.Discover()
     for d in devices.Value:
         print(d.IPAddress, d.ProductName, d.Revision)
+
+
+from fastapi import FastAPI
+from pylogix import PLC
+
+app = FastAPI()
+
+@app.get("/plcs")
+async def get_all_devices():
+    with PLC() as comm:  # No IP address needed for broadcasting
+        devices = comm.Discover()
+        devices_list = []
+        for d in devices.Value:
+            devices_list.append(d)
+    
+    return devices_list
